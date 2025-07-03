8ebf09e7744dc8f43b250a9c4c8663076a5b33cd
SreyaNaradasu <imsreyaa@gmail.com>
2025-07-03 17:24:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+from flask import Flask, render_template, request
+from scheduler import generate_timetable, time_slots
 
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    timetable = {}
+    semester = ''
+    section = ''
+    if request.method == "POST":
+        semester = request.form["semester"]
+        section = request.form["section"]
+        timetable = generate_timetable(semester, section)
+    return render_template("index.html", semester=semester, section=section, timetable=timetable, time_slots=time_slots)
+
+if __name__ == "__main__":
+    app.run(debug=True)
